6548eb031f31e88ab8c9f4b26e2ef18df626ea1a
IvayloK <ivaylokarapeykov@gmail.com>
2022-12-17 02:28:41 +0200

    (Expense) screen UI Progress

--- components/ExpensesList.tsx
+++ components/ExpensesList.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { theme } from "../theme";
 import { ExpensesGroup } from "../types/expensesGroup";
 import { ExpenseRow } from "./ExpenseRow";
@@ -9,12 +10,22 @@ type Props = {
 
 export const ExpensesList = ({ groups }: Props) => {
     return (
-        <View style={styles({ overflow: 'scroll' }).flexColumn}>
+        <View style={styles({ overflow: 'scroll', paddingTop: 16 }).flexColumn}>
+            <View style={styles({ marginBottom: 16, alignItems: 'center', justifyContent: 'center' }).flexRow}>
+                <Text style={{ color: theme.colors.text, fontSize: 17 }}>Total for: </Text>
+                <TouchableOpacity style={{ marginLeft: 25 }}>
+                    <Text style={{ color: theme.colors.primary, fontSize: 17 }}>This week</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles({ alignItems: 'flex-start', justifyContent: 'center', marginBottom: 16 }).flexRow}>
+                <Text style={{ color: theme.colors.text, fontSize: 32, fontWeight: '600' }}>159</Text>
+                <Text style={{ color: theme.colors.text, fontSize: 17, marginTop: 4, marginLeft: 2 }}>BGN</Text>
+            </View>
             {
                 groups.map(({ day, expenses, total }) => (
                     <View key={day} style={styles({ paddingHorizontal: 15, marginBottom: 24 }).flexColumn}>
                         <Text style={{ marginBottom: 4, color: theme.colors.textSecondary, fontSize: 17, fontWeight: '600' }}>{day}</Text>
-                        <View style={{ borderBottomColor: theme.colors.border, borderBottomWidth: StyleSheet.hairlineWidth, marginBottom: 12 }} />
+                        <View style={{ borderBottomColor: theme.colors.border, borderBottomWidth: StyleSheet.hairlineWidth, marginBottom: 8 }} />
                         {
                             expenses.map((expense) => (
                                 <ExpenseRow key={expense.id} expense={expense} />
